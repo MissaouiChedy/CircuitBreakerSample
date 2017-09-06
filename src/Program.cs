4dfbf2ebcf629100b45db3fd96ba0dc4a6183273
Chedy Missaoui <missaoui.chedy@gmail.com>
2017-09-06 10:48:51 +0100

    Refined the main program

--- src/Program.cs
+++ src/Program.cs
@@ -8,23 +8,39 @@ namespace CircuitBreakerSample
     {
         static void Main(string[] args)
         {
+            // Create a circuit breaker repository
             IPersonRepository repository = new CircuitBreakerRepository(DatabaseConnectionFactory.Connection);
             for (int i = 0 ; i < 100; i++) 
             {
                 try 
                 {
-                    repository.Write( new Person 
-                    {
-                        Id = i,
-                        Name = $"Roy {i}"
-                    });
-                    Console.WriteLine($"Written {i}");
+                    ReadOrWrite(repository, i);
                 }
                 catch (Exception e)
                 {
                     Console.WriteLine($"{e.GetType().FullName}: {e.Message}");
                 }
                 Thread.Sleep(800);
+            }
+        }
+        static void ReadOrWrite(IPersonRepository repository, int i) 
+        {
+            var random = new Random(50);
+            if (random.Next() > 25) 
+            {
+                // make a write
+                var person = new Person 
+                {
+                    Id = i,
+                    Name = $"Baby Roy {i}"
+                };
+                repository.Write(person);
+                Console.WriteLine($"Written: {person}");
+            }
+            else 
+            {
+                // make a read
+                Console.WriteLine(string.Join(", ", repository.Read()));
             }
         }
     }
